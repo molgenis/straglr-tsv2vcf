9450cb99f271a9abd210646a4bfd1ce1eb2ea649
bart.charbon <b.charbon@umcg.nl>
2026-01-23 10:47:11 +0100

    Process review comments

--- src/main/java/org/molgenis/vcf/straglr/model/LocusKey.java
+++ src/main/java/org/molgenis/vcf/straglr/model/LocusKey.java
@@ -3,6 +3,6 @@ package org.molgenis.vcf.straglr.model;
 /**
  * @param contig contig identifier
  * @param start 1 based start position
- * @param stop 1 based stop position
+ * @param stop 1 based, inclusive, stop position
  */
 public record LocusKey(String contig, int start, int stop) {}
