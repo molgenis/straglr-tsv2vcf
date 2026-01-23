765a84db1a1846bc34e4e58672d5f02b6d77b8c2
Bart Charbon <b.charbon@umcg.nl>
2026-01-23 07:55:03 +0100

    Update src/main/java/org/molgenis/vcf/straglr/model/Locus.java
    Co-authored-by: Dennis Hendriksen <d.hendriksen@umcg.nl>

--- src/main/java/org/molgenis/vcf/straglr/model/Locus.java
+++ src/main/java/org/molgenis/vcf/straglr/model/Locus.java
@@ -4,7 +4,7 @@ package org.molgenis.vcf.straglr.model;
  * @param chrom contig identifier
  * @param start 1 based start position, based on straglr loci (catalog) file
  * @param stop 1 based stop position, based on straglr loci (catalog) file
- * @param catalogRepeatUnit Repeat unit as specified in the straglr bed file
+ * @param catalogRepeatUnit Repeat unit as specified in the straglr loci (catalog) file
  * @param identifier Locus identifier from straglr loci (catalog) file
  */
 public record Locus(
