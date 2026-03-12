c38fc8a4371efef24610257fd5d7d84403a8fed2
bart.charbon <b.charbon@umcg.nl>
2026-03-12 09:56:24 +0100

    Fix warnings

--- src/main/java/org/molgenis/vcf/straglr/model/LocusKey.java
+++ src/main/java/org/molgenis/vcf/straglr/model/LocusKey.java
@@ -1,6 +1,8 @@
 package org.molgenis.vcf.straglr.model;
 
 /**
+ * Unique identifier for a locus
+ *
  * @param contig contig identifier
  * @param start 1 based start position
  * @param stop 1 based, inclusive, stop position
