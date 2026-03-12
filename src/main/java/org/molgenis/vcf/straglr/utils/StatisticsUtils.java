710cd19bc83fd3b79cf8d23104441aa710fe7d3e
bart.charbon <b.charbon@umcg.nl>
2026-03-12 09:56:40 +0100

    Fix locale specific test

--- src/main/java/org/molgenis/vcf/straglr/utils/StatisticsUtils.java
+++ src/main/java/org/molgenis/vcf/straglr/utils/StatisticsUtils.java
@@ -1,6 +1,9 @@
 package org.molgenis.vcf.straglr.utils;
 
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
 import java.util.List;
+import java.util.Locale;
 import org.apache.commons.math3.distribution.TDistribution;
 import org.apache.commons.math3.stat.descriptive.DescriptiveStatistics;
 import org.molgenis.vcf.straglr.model.Read;
@@ -29,6 +32,7 @@ public class StatisticsUtils {
     double lower = mean - margin;
     double upper = mean + margin;
 
-    return String.format("%.1f-%.1f", lower, upper);
+    DecimalFormat df = new DecimalFormat("0.0", new DecimalFormatSymbols(Locale.getDefault()));
+    return String.format("%s-%s", df.format(lower), df.format(upper));
   }
 }
